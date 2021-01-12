7fc3c24c49e7965bf0c910491c9e6918d7754f06
Toktam <toktam.ebadi@ga.gov.au>
2021-01-13 10:12:24 +1100

    Added a long run task.

---
+++ libs/stats/odc/stats/_cli_long_exec.py
@@ -0,0 +1,32 @@
+import time
+import numpy as np
+import click
+
+from ._cli_common import main
+from odc.aws.queue import get_messages, get_queue
+
+
+@main.command("mock-long-run")
+@click.option(
+    "--dryrun",
+    is_flag=True,
+    help="Do not run computation, from long execution test",
+)
+def mock_log_run(dryrun):
+    """
+       Mock long executing tasks
+    """
+    start_t = time.time()
+    sqs_queue = get_queue('deafrica-prod-eks-stats-geomedian')
+    for msg in get_messages(sqs_queue, visibility_timeout=1260):
+        while (datetime.now() - start_t) < 1200:
+            wl1 = np.random.normal(0,1,(100000,10000))
+            wl2 = np.random.normal(0,1,(100000,10000))
+            Wl3 = wl1 * wl2
+        msg.delete()
+
+    s3 = boto3.resource('s3')
+    object = s3.Object('deafrica-data', f'esa/s2/ga_s2_clear_pixel_count/0.0.1/test/{msg}.txt')
+    object.put(Body=b'some data')
+    print("DONE")
+
