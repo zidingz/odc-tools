64bf9265e14e5fc59e2e2b3b4695ca392b8bce43
Toktam <toktam.ebadi@ga.gov.au>
2021-01-14 14:41:12 +1100

    Add logging info.

--- libs/stats/odc/stats/_cli_long_exec.py
+++ libs/stats/odc/stats/_cli_long_exec.py
@@ -2,11 +2,12 @@ import time
 import os
 import numpy as np
 import boto3
+ import logging
 
 import click
 from datetime import datetime
 
-from ._cli_common import main
+from ._cli_common import main, setup_logging
 from odc.aws.queue import get_messages, get_queue
 
 
@@ -20,12 +21,14 @@ def mock_log_run(dryrun):
     """
        Mock long executing tasks
     """
+    setup_logging()
+    _log = logging.getLogger(__name__)
     start_t = datetime.now()
     os.environ['RUN_STATUS'] = '0'
     sqs_queue = get_queue('deafrica-prod-eks-stats-geomedian')
     for msg in get_messages(sqs_queue, visibility_timeout=1200):
-        print(msg.body)
-        while (datetime.now() - start_t).total_seconds() < 180:
+        _log.info(f'procesing {msg.body}')
+        while (datetime.now() - start_t).total_seconds() < 600:
             wl1 = np.random.normal(0,1,(100000,10000))
             wl2 = np.random.normal(0,1,(100000,10000))
             Wl3 = wl1 * wl2
@@ -33,6 +36,6 @@ def mock_log_run(dryrun):
         s3 = boto3.resource('s3')
         object = s3.Object('deafrica-data', f'esa/s2/ga_s2_clear_pixel_count/0.0.1/test/{msg.body}.txt')
         object.put(Body=b'some data')
-        print(f"wrote esa/s2/ga_s2_clear_pixel_count/0.0.1/test/{msg.body}.txt to s3://deafrica-data")
+        _log.info(f"wrote esa/s2/ga_s2_clear_pixel_count/0.0.1/test/{msg.body}.txt to s3://deafrica-data")
         os.environ['RUN_STATUS'] = '1'
         msg.delete()
