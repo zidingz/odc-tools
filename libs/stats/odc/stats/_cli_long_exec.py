e8b52bf439a81ba235cafde4c24cd7a42231a576
Toktam <toktam.ebadi@ga.gov.au>
2021-01-14 11:07:27 +1100

    Add termination signal as environment variable.

--- libs/stats/odc/stats/_cli_long_exec.py
+++ libs/stats/odc/stats/_cli_long_exec.py
@@ -1,4 +1,5 @@
 import time
+import os
 import numpy as np
 import boto3
 
@@ -21,16 +22,17 @@ def mock_log_run(dryrun):
     """
     start_t = datetime.now()
     sqs_queue = get_queue('deafrica-prod-eks-stats-geomedian')
-    for msg in get_messages(sqs_queue, visibility_timeout=1260):
+    for msg in get_messages(sqs_queue, visibility_timeout=1200):
         print(msg.body)
-        while (datetime.now() - start_t).total_seconds() < 1:
+        while (datetime.now() - start_t).total_seconds() < 600:
             wl1 = np.random.normal(0,1,(100000,10000))
             wl2 = np.random.normal(0,1,(100000,10000))
             Wl3 = wl1 * wl2
 
-        msg.delete()
         s3 = boto3.resource('s3')
         object = s3.Object('deafrica-data', f'esa/s2/ga_s2_clear_pixel_count/0.0.1/test/{msg.body}.txt')
         object.put(Body=b'some data')
         print(f"wrote esa/s2/ga_s2_clear_pixel_count/0.0.1/test/{msg.body}.txt to s3://deafrica-data")
+        os.environ['RUN_STATUS'] = '1'
+        msg.delete()
 
